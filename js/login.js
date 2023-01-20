f680b1b239218097620b95739be535075734d6ed
JimenaGitHub <jaliagasanhueza@gmail.com>
2023-01-20 20:24:28 -0300

    good practice js

---
+++ js/login.js
@@ -0,0 +1,8 @@
+// Calling this function
+
+function login(){
+    if (document.getElementById("usuario").value == ""
+    || document.getElementById("pass").value == ""){
+      alert("Usuario y clave vacíos")
+    }
+  }
---
+++ js/logup.js
@@ -0,0 +1,8 @@
+// Calling the log in function validation.
+
+function logup(){
+    if (document.getElementById("usuario").value == ""
+    || document.getElementById("pass").value == ""){
+      alert("Usuario y clave vacíos")
+    }
+  }
