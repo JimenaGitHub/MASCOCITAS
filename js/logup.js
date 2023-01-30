d5ccc4b0dec4efcd82e59e14c5f03c0a3698f18e
JimenaGitHub <jaliagasanhueza@gmail.com>
2023-01-30 18:59:57 -0300

    improvements

--- js/logup.js
+++ js/logup.js
@@ -1,8 +1,12 @@
 // Calling the log in function validation.
 
 function logup(){
-    if (document.getElementById("usuario").value == ""
-    || document.getElementById("pass").value == ""){
-      alert("Usuario y clave vacíos")
+    if (document.getElementById("nombre").value == ""
+    || document.getElementById("apellido").value == ""
+    || document.getElementById("correo").value == ""
+    || document.getElementById("pass").value == ""
+    || document.getElementById("pass2").value == "")
+    {
+      alert("nombre, apellido, correo, clave y repetir clave están vacíos")
     }
-  }
+}
